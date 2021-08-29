e176e03be0edec8b4578af9ce614192174312b9d
sankassio99 <kciosantana@gmail.com>
2021-08-29 17:40:10 -0300

    first

---
+++ api-meu-blog/app/Http/Controllers/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Posts;
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+class Controller extends BaseController
+{
+    public function index()
+    {
+        // return User::findOrFail($id);
+        
+        return Posts::all();
+    }
+
+    public function store(Request $request){
+        return response()->json(Posts::create([
+            "titulo" => $request->titulo,
+            "conteudo" => $request->conteudo], 201)
+        );
+    }
+}
