6c1701aa61bef14d282768aa4376ccc6993a2bdd
sankassio99 <kciosantana@gmail.com>
2021-10-31 03:59:22 -0300

    feature: nova rota de edicao de post

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,8 +10,6 @@ class Controller extends BaseController
 {
     public function index()
     {
-        // return User::findOrFail($id);
-        
         return Posts::all();
     }
 
@@ -20,10 +18,30 @@ class Controller extends BaseController
     }
 
     public function store(Request $request){
-        return response()->json(Posts::create([
-            "titulo" => $request->titulo,
-            "conteudo" => $request->conteudo], 201)
-        );
+        return response()->json(Posts::create($request->all(), 201));
+    }
+
+    public function destroy($id){
+        $qtdRecursosRemovidos = Posts::destroy($id);
+
+        if($qtdRecursosRemovidos === 0){
+            return response()->json(["erro"=> "Recurso não encontrado"], 204);
+        }
+
+        return response()->json("removido com sucesso");
+    }
+
+    public function update(int $id, Request $request){
+        $post = Posts::find($id);
+
+        if($post==null){
+            return response()->json(["erro" => "Recurso não encontrado"], 404) ;
+        }
+        $post->fill($request->all());
+        $post->save();
+
+        return response()->json($post) ;
+        
     }
 
     public function welcome(){
